49d6e7ebabee70a47ef8a404e00a4c3be4455714
Juan Cruz Dal Bello <98571416+jcdalbello@users.noreply.github.com>
2024-07-18 20:02:39 -0300

    Update light-dark.bat

--- light-dark.bat
+++ light-dark.bat
@@ -3,7 +3,7 @@ REM Set key values
 set KEY_NAME=HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize
 set VALUE_NAME=AppsUseLightTheme
 
-REM get the current value to check which mode is active
+REM Get the current value to check which mode is active
 FOR /F "skip=2 tokens=2,*" %%A IN ('reg.exe query %KEY_NAME% /v %VALUE_NAME%') DO set "VALUE=%%B"
 
 set NEW_VALUE=0x0
@@ -18,6 +18,6 @@ reg add %KEY_NAME% /v %VALUE_NAME% /t REG_DWORD /d %NEW_VALUE% /f
 set VALUE_NAME=SystemUsesLightTheme
 reg add %KEY_NAME% /v %VALUE_NAME% /t REG_DWORD /d %NEW_VALUE% /f
 
-REM kill and restart explorer.exe to update the taskbar color
+REM Kill and restart explorer.exe to update the taskbar color
 taskkill /im explorer.exe /f
 start explorer.exe
